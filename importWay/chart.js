c99a7693ef4e5f98157825914ed82e215288fc6f
Annie <dh10050160@gmail.com>
2023-08-27 16:37:51 +0800

    chartjs-maincase

--- importWay/chart.js
+++ importWay/chart.js
@@ -1,36 +1,38 @@
-
-
+import { json } from './load.js';
+import { compareTOP3 } from './compare.js';
+const selectcase = document.getElementById("kmlFileSelect");
+//初始化chart
 const myChart = new Chart(document.getElementById("myChart"), {
     type: 'bar',
     data: {
         labels: ['1', '3', '6', '12', '24'],
         datasets: [
             {
-            label: '99凡那比颱風',
-            data: [124, 321, 561, 824.5, 934.5],
+            label: '',
+            data: [],
             borderWidth: 1,
             backgroundColor: 'rgba(255, 0, 0, 0.8)',//紅色
             },
             {
-            label: '105梅姬颱風',
-            data: [83, 163, 256.5, 443, 639.5],
+            label: '',
+            data: [],
             borderWidth: 1,
             backgroundColor: 'rgba(0,100,0,0.8)'//深綠色
             },
             {
-            label: '98莫拉克颱風',
-            data: [0, 0, 0, 0, 633.5],
+            label: '',
+            data: [],
             borderWidth: 1,
             backgroundColor: 'rgba(0, 0, 255, 0.8)'//藍色
             },
             {
-            label: '1070823豪雨',
-            data: [107, 229, 292, 412, 559],
+            label: '',
+            data: [],
             borderWidth: 1,
             backgroundColor: 'rgb(255,140,0,0.8)'//橘色
             }
         ]
-        },
+    },
     options: {
         maintainAspectRatio: false,
         title:{
@@ -50,22 +52,104 @@ const myChart = new Chart(document.getElementById("myChart"), {
                 scaleLabel: {
                     display: true,
                     labelString: '雨量(mm)'
-                }
-            }],
-            //坐標軸刻度
-            /*
-            x: [{
+                },
                 ticks: {
-                    autoSkip: false,//不自動跳過刻度
-                    maxRotation: 45,//文字傾斜
-                    minRotation: 45//文字傾斜
+                    suggestedMax: 1000
                 }
-            }],
-            */
-            y: {
-                beginAtZero: true, //從0開始
-            }
+            }]
         }
+    }
+});
+let selectcaseARR = [];
+addEventListener("change", function(event) {
+    if (event.target === selectcase) {
+        const id = selectcase.options[select.selectedIndex].id//取得選項的 ID
         
+        //初始化chart
+        myChart.data.datasets[0].label = '';
+        myChart.update();
+
+        selectcaseARR = [];
+        myChart.data.datasets[0].data= selectcaseARR;
+
+        myChart.data.datasets[0].label = json[id-1].name;//main事件的名稱
+
+        selectcaseARR.push(json[id-1].hr1);
+        selectcaseARR.push(json[id-1].hr3);
+        selectcaseARR.push(json[id-1].hr6);
+        selectcaseARR.push(json[id-1].hr12);
+        selectcaseARR.push(json[id-1].hr24);
+        myChart.data.datasets[0].data= selectcaseARR;//main事件的雨量
+        myChart.update();
+
+        /*
+        const myChart = new Chart(document.getElementById("myChart"), {
+            type: 'bar',
+            data: {
+                labels: ['1', '3', '6', '12', '24'],
+                datasets: [
+                    {
+                    label: '99凡那比颱風',
+                    data: [124, 321, 561, 824.5, 934.5],
+                    borderWidth: 1,
+                    backgroundColor: 'rgba(255, 0, 0, 0.8)',//紅色
+                    },
+                    {
+                    label: '105梅姬颱風',
+                    data: [83, 163, 256.5, 443, 639.5],
+                    borderWidth: 1,
+                    backgroundColor: 'rgba(0,100,0,0.8)'//深綠色
+                    },
+                    {
+                    label: '98莫拉克颱風',
+                    data: [0, 0, 0, 0, 633.5],
+                    borderWidth: 1,
+                    backgroundColor: 'rgba(0, 0, 255, 0.8)'//藍色
+                    },
+                    {
+                    label: '1070823豪雨',
+                    data: [107, 229, 292, 412, 559],
+                    borderWidth: 1,
+                    backgroundColor: 'rgb(255,140,0,0.8)'//橘色
+                    }
+                ]
+                },
+            options: {
+                maintainAspectRatio: false,
+                title:{
+                    display: true,
+                    text: '累積雨量',
+                    fontSize: "20"
+                },
+                scales: {
+                    //坐標軸標題
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: '時間(小時)'
+                        }
+                    }],
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: '雨量(mm)'
+                        }
+                    }],
+                    //坐標軸刻度
+                    x: [{
+                        ticks: {
+                            autoSkip: false,//不自動跳過刻度
+                            maxRotation: 45,//文字傾斜
+                            minRotation: 45//文字傾斜
+                        }
+                    }],
+                    y: {
+                        beginAtZero: true, //從0開始
+                    }
+                }
+                
+            }
+        });
+        */
     }
 });
--- importWay/compare.js
+++ importWay/compare.js
@@ -3,56 +3,65 @@ let json = window.load.data;
 let arrcase = [];
 // 宣告全域物件
 window.compare = window.compare || {};
-addEventListener("change", function() {
-    const select = document.getElementById("kmlFileSelect");//取得HTML的select元素
-    const id = select.options[select.selectedIndex].id//取得選項的 ID
-    //let num = id-1; // 選項的 ID-1是為了後續迴圈的索引值
-    // 取得表格元素
-    const table = document.querySelector(".table");
-    // 取得並更改欄元素
-    table.rows[1].cells[1].textContent = json[id-1].name; // main事件的名稱
-    table.rows[1].cells[2].textContent = json[id-1].hr24; // main事件的24hr(mm)
-    table.rows[1].cells[3].textContent = json[id-1].AREA; // main事件的AREA
-    table.rows[1].cells[4].textContent = json[id-1].depth; // main事件的depth
-    
-    //找出三個24hr最接近的事件
-    // step1: 將json中所有事件的hr24與main事件的hr24相減，並取絕對值
-    let arr24hr = [];
-    for(let i=0;i<json.length;i++){
-        arr24hr.push(Math.abs(json[i].hr24-json[id-1].hr24));//絕對值            
-    }
-    // console.log("arr24hr:"+arr24hr);
+const selectcase = document.getElementById("kmlFileSelect");//取得HTML的select元素
+addEventListener("change", function(event) {
+    if (event.target === selectcase) {
+        const id = selectcase.options[select.selectedIndex].id//取得選項的 ID
+        //let num = id-1; // 選項的 ID-1是為了後續迴圈的索引值
+        // 取得表格元素
+        const table = document.querySelector(".table");
+        //初始化表格    
+        for(let i=0;i<5;i++){
+            table.rows[i].cells[1].textContent = "";
+            table.rows[i].cells[2].textContent = "";
+            table.rows[i].cells[3].textContent = "";
+            table.rows[i].cells[4].textContent = "";
+        }
+        // 取得並更改欄元素
+        table.rows[1].cells[1].textContent = json[id-1].name; // main事件的名稱
+        table.rows[1].cells[2].textContent = json[id-1].hr24; // main事件的24hr(mm)
+        table.rows[1].cells[3].textContent = json[id-1].AREA; // main事件的AREA
+        table.rows[1].cells[4].textContent = json[id-1].depth; // main事件的depth
+        
+        //找出三個24hr最接近的事件
+        // step1: 將json中所有事件的hr24與main事件的hr24相減，並取絕對值
+        let arr24hr = [];
+        for(let i=0;i<json.length;i++){
+            arr24hr.push(Math.abs(json[i].hr24-json[id-1].hr24));//絕對值            
+        }
+        // console.log("arr24hr:"+arr24hr);
 
-    // step2: 在arr24hr找出與json[id-1].hr24差值最小的三個
-    let arrtop3 = [];
-    for(let i=0;i<4;i++){
-        let min = Math.min(...arr24hr);//找出arr24hr中最小的數字
-        arrtop3.push(min);//將最小的數字放入arrtop3
-        arr24hr.splice(arr24hr.indexOf(min),1);//將arr中最小的數字刪除
-    }
-    // console.log("arrtop3:"+arrtop3);
+        // step2: 在arr24hr找出與json[id-1].hr24差值最小的三個
+        let arrtop3 = [];
+        for(let i=0;i<4;i++){
+            let min = Math.min(...arr24hr);//找出arr24hr中最小的數字
+            arrtop3.push(min);//將最小的數字放入arrtop3
+            arr24hr.splice(arr24hr.indexOf(min),1);//將arr中最小的數字刪除
+        }
+        // console.log("arrtop3:"+arrtop3);
 
-    // step3: 找出arrtop3相對應的json[id-1].hr24的index
-    arrcase = []; // 初始化arrcase
-    for(let i=1;i<4;i++){
-        for(let j=0;j<json.length;j++){
-            if(Math.abs(json[j].hr24-json[id-1].hr24) == arrtop3[i]){
-                arrcase.push(j+1);//no
-                table.rows[i+1].cells[1].textContent = json[j].name;
-                table.rows[i+1].cells[2].textContent = json[j].hr24;
-                table.rows[i+1].cells[3].textContent = json[j].AREA;
-                table.rows[i+1].cells[4].textContent = json[j].depth;
+        // step3: 找出arrtop3相對應的json[id-1].hr24的index
+        arrcase = []; // 初始化arrcase
+        for(let i=1;i<4;i++){
+            for(let j=0;j<json.length;j++){
+                if(Math.abs(json[j].hr24-json[id-1].hr24) == arrtop3[i]){
+                    arrcase.push(j+1);//no
+                    table.rows[i+1].cells[1].textContent = json[j].name;
+                    table.rows[i+1].cells[2].textContent = json[j].hr24;
+                    table.rows[i+1].cells[3].textContent = json[j].AREA;
+                    table.rows[i+1].cells[4].textContent = json[j].depth;
+                }
             }
         }
+        //初始化window.compare.arrcase
+        window.compare.arrcase = [];
+        // 將資料添加到全域物件中
+        window.compare.arrcase = arrcase;
+        // console.log("compare-windowCompare:"+window.compare.arrcase);
+        // console.log("arrcase:"+arrcase);
+        var event = new Event('dataUpdated'); // 建立事件
+        document.dispatchEvent(event); // 觸發事件
     }
-    //初始化window.compare.arrcase
-    window.compare.arrcase = [];
-    // 將資料添加到全域物件中
-    window.compare.arrcase = arrcase;
-    // console.log("compare-windowCompare:"+window.compare.arrcase);
-    // console.log("arrcase:"+arrcase);
-    var event = new Event('dataUpdated'); // 建立事件
-    document.dispatchEvent(event); // 觸發事件
 });
 export let compareTOP3 = arrcase;
 // 不行寫在事件外面，因為事件是異步的，會先執行完事件外面的程式碼，再執行事件內的程式碼
